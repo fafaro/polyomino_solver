acea91e91ca1d325931372dabe2f82984dd727c1
Furqan Farooqui <furqanfarooqui@gmail.com>
2018-05-06 09:37:01 -0500

    Added more missing files.

---
+++ ui/PuzzleModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ui
+{
+    class PuzzleModel
+    {
+        private polyomino_solver.Puzzle puzzle = new polyomino_solver.Puzzle();
+
+        public polyomino_solver.Point BoardSize
+        {
+            get { return puzzle.BoardSize; }
+            set { puzzle.BoardSize = value; }
+        }
+
+        public polyomino_solver.Piece[] Pieces
+        {
+            get { return puzzle.Pieces; }
+            set { puzzle.Pieces = value; }
+        }
+
+        public void Solve()
+        {
+            solution = puzzle.Solve(puzzle.BuildMatrix());
+        }
+
+        private polyomino_solver.Puzzle.SolutionPiece[] solution = null;
+        public polyomino_solver.Puzzle.SolutionPiece[] Solution
+        {
+            get
+            {
+                return solution;
+            }
+        }
+    }
+}
